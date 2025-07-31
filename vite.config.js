7534556be621e851a091d59aea8be2fed5b19e11
magesharun <magesharun2016@gmail.com>
2025-07-31 21:53:34 +0530

    ss

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/velloredental",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],  // ONLY react plugin here
+    base: env.VITE_BASE_PATH || '/velloredental',
+  }
 })
