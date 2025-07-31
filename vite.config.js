58f907e9e75b872842993c5c2d2d0b8c9dd5403b
magesharun <magesharun2016@gmail.com>
2025-07-31 21:16:02 +0530

    ss

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || '/velloredental',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, import.meta.env.MODE)
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || '/velloredental',
+  }
 })
